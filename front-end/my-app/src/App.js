b093aa5c3f57dacf8dac636cb120fd6613eb83f1
Ryan Nourbaran <ryan.nourbaran@hotmail.com>
2018-09-27 22:50:51 -0400

    added more front-end

--- front-end/my-app/src/App.js
+++ front-end/my-app/src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo2.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 
 class App extends Component {
   render() {
@@ -8,11 +10,70 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Housing Price Estimator</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Form>
+          <FormGroup>
+            <Label for="roomSelect">Number of Rooms</Label>
+            <Input type="select" name="numRooms" id="roomSelect">
+              <option>1-2</option>
+              <option>3-4</option>
+              <option>5-6</option>
+              <option>7-9</option>
+              <option>10+</option>
+            </Input>
+          </FormGroup>
+          <FormGroup>
+            <Label for="bedroomSelect">Number of Bedrooms</Label>
+            <Input type="select" name="numBedrooms" id="bedroomSelect">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Input>
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="exampleText">Text Area</Label>
+            <Input type="textarea" name="text" id="exampleText" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleFile">File</Label>
+            <Input type="file" name="file" id="exampleFile" />
+            <FormText color="muted">
+              This is some placeholder block-level help text for the above
+              input. It's a bit lighter and easily wraps to a new line.
+            </FormText>
+          </FormGroup>
+          <FormGroup tag="fieldset">
+            <legend>Radio Buttons</legend>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio1" /> Option one is this and
+                that—be sure to include why it's great
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="radio" name="radio1" /> Option two can be something
+                else and selecting it will deselect option one
+              </Label>
+            </FormGroup>
+            <FormGroup check disabled>
+              <Label check>
+                <Input type="radio" name="radio1" disabled /> Option three is
+                disabled
+              </Label>
+            </FormGroup>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input type="checkbox" /> Check me out
+            </Label>
+          </FormGroup>
+          <Button>Submit</Button>
+        </Form>
       </div>
     );
   }
